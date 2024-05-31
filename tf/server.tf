19d48181f5ee9f6fab92c2170776be5e34e2f0d9
max <30047099+mxtw@users.noreply.github.com>
2024-05-31 18:39:55 +0200

    feat: k3s cloud init

--- tf/server.tf
+++ tf/server.tf
@@ -4,18 +4,29 @@ terraform {
       source  = "registry.terraform.io/hetznercloud/hcloud"
       version = "~> 1.47"
     }
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "~> 3.6"
+    }
   }
+  required_version = "~> 1.7"
 }
 
 variable "hcloud_token" {
-    sensitive = true
+  type = string
+  sensitive = true
+}
+
+resource "random_string" "k3s_token" {
+  length  = 48
+  special = false
 }
 
 provider "hcloud" {
   token = var.hcloud_token
 }
 
-resource "hcloud_server" "server" {
+resource "hcloud_server" "main" {
   name        = "k3s-1"
   server_type = "cx21"
   image       = "ubuntu-24.04"
@@ -26,4 +37,11 @@ resource "hcloud_server" "server" {
     ipv4_enabled = true
     ipv6_enabled = true
   }
+
+  user_data = templatefile("${path.module}/templates/init.sh",
+    {
+      k3s_channel = "stable"
+      k3s_token   = "{resource.random_string.k3s_token}"
+    }
+  )
 }
---
+++ tf/templates/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get -yq update
+apt-get install -yq \
+    ca-certificates \
+    curl \
+    ntp
+
+# k3s
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - \
+    --flannel-backend=host-gw \
+    --kubelet-arg 'cloud-provider=external'
